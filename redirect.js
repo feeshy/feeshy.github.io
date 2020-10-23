9651e629fcfa98d4b7ac709c6b2e4d09fa81c2fe
feeshy <10860212+feeshy@users.noreply.github.com>
2020-10-23 17:27:53 +0800

    url redirect

--- redirect.js
+++ redirect.js
@@ -17,20 +17,4 @@ window.onload=function() {
             document.location = url.replace('px_pt_dp.html', 'css_width.html')
             break
     }
-//    if (url.includes('/search')) {
-//        document.location = url.replace('/search', '/s')
-//    }
-//    if (url.includes('/ambient')) {
-//        document.location = url.replace('/ambient', '/audio')
-//    }
-//    if (url.includes('/tech/free_audio_plugins.html')) {
-//        document.location = url.replace('/tech/free_audio_plugins.html', '/audio/free_audio_plugins.html')
-//    }
-//    if (url.includes('/tech/free_virtual_instruments.html')) {
-//        document.location = url.replace('/tech/free_virtual_instruments.html', '/audio/free_virtual_instruments.html')
-//    }
-//    if (url.includes('px_pt_dp.html')) {
-//        document.location = url.replace('px_pt_dp.html', 'css_width.html')
-//    }
-    
 }
