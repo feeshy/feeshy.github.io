df0abc072ba9ec4ea85fbfc73663a000b641e05c
feeshy <10860212+feeshy@users.noreply.github.com>
2020-11-11 14:46:25 +0800

    update logic of code

--- tech/ppi_calc.js
+++ tech/ppi_calc.js
@@ -12,6 +12,8 @@ function calc() {
     inchD = document.getElementById("inchD").value
 
     if (inchD != 0) {
+
+        //calculate ppi & physical size
         
         var ppi, scaledPPI, mmW, mmH, equivPPI
         ppi = Math.sqrt(Math.pow(pixelW, 2) + Math.pow(pixelH, 2)) / inchD
@@ -28,55 +30,73 @@ function calc() {
         document.getElementById("cmD").innerHTML = "（" + Math.round(inchD * 25.4) / 10 + " 厘米）"
         document.getElementById("result").innerHTML = "PPI=" + Math.round(equivPPI)
         
+        //calculate scale factors
+
         var mmShorter, scaleFacLgr, scaleFacMore, meterRetina, meterImmerse, meterMax
         if (parseFloat(mmW) <= parseFloat(mmH)) {
             mmShorter = mmW
         }   else {
             mmShorter = mmH
         }
+
         switch (true) {
         case mmShorter >= 40 && mmShorter < 90:         //Mobile Phone
             var phoneFactors = [/*240p*/0.75, /*320p*/1.0, /*480p*/1.5, /*576p,640p,720p*/2.0, /*720p,SurfaceDuo*/2.5, /*1080p*/2.625, /*1080p*/2.75, /*1080p,palm*/3.0, /*1440p*/3.5, /*1440p*/3.66, /*1440p*/4.0, /*2160p*/5.25, /*2160p*/5.5, /*2160p*/6.0]
             scaleFacLgr = roundToArray(ppi / 144, phoneFactors) * 100
             scaleFacMore = roundToArray(ppi / 160, phoneFactors) * 100
-            document.getElementById("officeScale").style.cssText = "visibility:hidden;"
             break
         case mmShorter >= 90 && mmShorter < 135:        //7~9" Tab
             scaleFacLgr = Math.round(ppi / 132 * 4) * 25
             scaleFacMore = Math.round(ppi / 150 * 4) * 25
-            document.getElementById("officeScale").style.cssText = "visibility:hidden;"
             break
         case mmShorter >= 135 && mmShorter < 155:       //10" Tab
             scaleFacLgr = Math.round(ppi / 128 * 4) * 25
             scaleFacMore = Math.round(ppi / 144 * 4) * 25
-            document.getElementById("officeScale").style.cssText = "visibility:inherit;"
             break
         case mmShorter >= 155 && mmShorter < 245:       //Laptop
             scaleFacLgr = Math.round(ppi / 120 * 4) * 25
             scaleFacMore = Math.round(ppi / 135 * 4) * 25
-            document.getElementById("officeScale").style.cssText = "visibility:inherit;"
             break
         case mmShorter >= 245 && mmShorter < 450:       //Desktop Monitor
             scaleFacLgr = Math.round(ppi / 96 * 4) * 25
             scaleFacMore = Math.round(ppi / 120 * 4) * 25
-            document.getElementById("officeScale").style.cssText = "visibility:inherit;"
 /*          meterRetina = 
             meterImmerse = 
             meterMAX = 
 */          break
         case mmShorter >= 450:                          //TV, Projector, IMAX...
             scaleFacMore = scaleFacLgr = 100
-            document.getElementById("officeScale").style.cssText = "visibility:hidden;"
 /*          meterRetina = 
             meterImmerse = 
             meterMAX = 
 */          break
         default:
             scaleFacMore = scaleFacLgr = 100
-            document.getElementById("officeScale").style.cssText = "visibility:hidden;"
         }
 
-        if (scaleFacLgr > 100) {        // hidpi device
+        // this device section
+
+        var isThisDevice = Math.abs(pixelW - Math.round(window.screen.width * window.devicePixelRatio)) + Math.abs(pixelH - Math.round(window.screen.height * window.devicePixelRatio))             // this will allow deviations
+        if (  isThisDevice <= 2 ) {
+            document.getElementById("cssRes").innerHTML = window.screen.width + "×" + window.screen.height
+            var ua = navigator.userAgent
+            switch (true) {
+                case ua.includes('Windows NT 10'):
+                case ua.includes('Windows NT 6'):
+                    document.getElementById("officeScaleFac").innerHTML = Math.round(scaledPPI / 96 * 100) + "%"
+                    document.getElementById("officeScale").style.cssText = "visibility:inherit;"
+                    break
+                default:
+                    document.getElementById("officeScale").style.cssText = "visibility:hidden;"
+            }
+            document.getElementById("thisDevice").style.cssText = "visibility:visible;"
+        }   else {
+            document.getElementById("thisDevice").style.cssText = "visibility:hidden;"
+        }
+
+        // recommend seciton
+
+        if (scaleFacLgr > 100) {                  //hidpi device
             document.getElementById("tooLow").innerHTML = ""
             
             if (scaleFacMore < 100) {
@@ -87,19 +107,11 @@ function calc() {
             document.getElementById("resLgr").innerHTML = Math.round(pixelW / scaleFacLgr * 100) + "x" + Math.round(pixelH / scaleFacLgr * 100)
             document.getElementById("resMore").innerHTML = Math.round(pixelW / scaleFacMore * 100) + "x" + Math.round(pixelH / scaleFacMore * 100)
             
-            var isThisDevice = Math.abs(pixelW - Math.round(window.screen.width * window.devicePixelRatio)) + Math.abs(pixelH - Math.round(window.screen.height * window.devicePixelRatio))
-            if (  isThisDevice <= 2 ) {
-                document.getElementById("cssRes").innerHTML = window.screen.width + "×" + window.screen.height
-                document.getElementById("officeScaleFactor").innerHTML = Math.round(scaledPPI / 96 * 100) + "%"
-                document.getElementById("thisDevice").style.cssText = "visibility:inherit;"
-            }   else {
-                document.getElementById("thisDevice").style.cssText = "visibility:hidden;"
-            }
             document.getElementById("recommend").style.cssText = "visibility:visible;"
 
         }   else {                  //lowdpi device
             document.getElementById("recommend").style.cssText = "visibility:hidden;"
-            switch (true) {          //too low
+            switch (true) {          //dpi too low
             case ppi < 42 && mmShorter >= 450:
                 document.getElementById("tooLow").innerHTML = "<br>建议拉远距离观看"
                 break
@@ -110,7 +122,7 @@ function calc() {
                 document.getElementById("tooLow").innerHTML = ""
             }
         }
-    }   else {          //diagonal not set
+    }   else {          //if diagonal size not set
         document.getElementById("recommend").style.cssText = "visibility:hidden;"
         document.getElementById("result").innerHTML = "请输入屏幕对角线长度"
     }
@@ -118,9 +130,11 @@ function calc() {
 
 document.addEventListener("change", calc)
 
-//  Thx to Gav & ncepuzs for the following function
-//  https://www.gavsblog.com/blog/find-closest-number-in-array-javascript
-//  https://meta.appinn.net/t/javascript/19118/3
+/*
+  Thx to Gav & ncepuzs for the following function
+  https://www.gavsblog.com/blog/find-closest-number-in-array-javascript
+  https://meta.appinn.net/t/javascript/19118/3
+*/
 
 function roundToArray(needle, haystack) {
     return haystack.reduce((a, b) => {
