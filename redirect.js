134b7014a190be437f9f9b7ed68f0ccc3810cdeb
feeshy <10860212+feeshy@users.noreply.github.com>
2023-08-02 21:18:42 +0800

    an index of sony live wallpaper packages

--- redirect.js
+++ redirect.js
@@ -7,9 +7,9 @@ window.onload=function() {
         case url.includes('/ambient'):
             document.location = url.replace('/ambient', '/sounds')
             break
-        case url.includes('/tech/free_audio_plugins.html'):
-        case url.includes('/tech/free_virtual_instruments.html'):
-            document.location = url.replace('/tech', '/sounds')
+        case url.includes('/sounds/free_audio_plugins.html'):
+        case url.includes('/sounds/free_virtual_instruments.html'):
+            document.location = url.replace('/sounds', '/tech')
             break
         case url.includes('px_pt_dp.html'):
             document.location = url.replace('px_pt_dp', 'css_width')
