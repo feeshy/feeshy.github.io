235d1aeb323b1dc5cf3234575433df754768fba0
feeshy <10860212+feeshy@users.noreply.github.com>
2026-05-24 23:34:29 +0800

    Update cache strategy

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,9 @@
-// 0. 接管页面控制权
+// 1. 接管页面控制权
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
-// 1. 预缓存规则
+// 2. 预缓存规则
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
-
-// =========================================================================
-// 2. 子 PWA 独立作用域避让规则
-// =========================================================================
-workbox.routing.registerRoute(
-    ({ url }) => url.pathname.startsWith('/compact-phones/') ||
-        url.pathname.startsWith('/ingress-tools/') ||
-        url.pathname.startsWith('/ppi-calc/') ||
-        url.pathname.startsWith('/simple-image-stitcher/'),
-    new workbox.strategies.NetworkOnly()
-);
 
 // =========================================================================
 // 3. 运行时全自动缓存路由
