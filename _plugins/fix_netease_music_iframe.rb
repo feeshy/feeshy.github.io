ba497a30fc460aa8be191822c179683041618130
feeshy <10860212+feeshy@users.noreply.github.com>
2026-08-11 15:27:45 +0800

    fix netease music iframe

--- _plugins/fix_netease_music_iframe.rb
+++ _plugins/fix_netease_music_iframe.rb
@@ -1,7 +1,15 @@
-Jekyll::Hooks.register [:posts, :pages], :post_render do |doc|
-  if doc.output_ext == ".html"
-    doc.output.gsub!(/<iframe[^>]*music\.163\.com\/outchain\/player[^>]*>/i) do |iframe|
-      iframe.gsub('&amp;', '&')
+$total_amp_count = 0
+
+[:posts, :pages, :documents].each do |owner|
+  Jekyll::Hooks.register owner, :post_render do |doc|
+    next unless doc.output_ext == ".html" && doc.output
+
+    doc.output.gsub!(/<iframe[\s\S]*?music\.163\.com[\s\S]*?>/i) do |iframe|
+      iframe.gsub('&amp;') { $total_amp_count += 1; '&' }
     end
   end
+end
+
+Jekyll::Hooks.register :site, :post_write do
+  Jekyll.logger.info "Fix-Iframe:", "共修复了 #{$total_amp_count} 个网易云音乐iframe"
 end
